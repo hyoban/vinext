f72777e272af575b3d1eb19d008240e04c856ed8
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-03-18 16:48:46 +0800

    exclude deps

--- examples/fumadocs-docs-template/vite.config.ts
+++ examples/fumadocs-docs-template/vite.config.ts
@@ -5,6 +5,28 @@ import fumadocsMdx from "fumadocs-mdx/vite";
 import * as sourceConfig from "./source.config";
 
 export default defineConfig(async () => ({
+  // we do this to avoid Vite from bundling React contexts and cause duplicated contexts conflicts.
+  optimizeDeps: {
+    exclude: [
+      'fumadocs-ui',
+      'fumadocs-core',
+      '@unpic/react',
+      '@unpic/core',
+      'unpic'
+    ],
+    include: [
+      'fumadocs-ui > debug',
+      'fumadocs-core > extend',
+      '@mdx-js/rollup > extend',
+      'fumadocs-mdx > extend',
+      'fumadocs-core > style-to-js',
+      '@mdx-js/rollup > style-to-js',
+      'fumadocs-mdx > style-to-js',
+    ],
+  },
+  ssr: {
+    external: ['@takumi-rs/image-response'],
+  },
   plugins: [
     tailwindcss(),
     await fumadocsMdx(sourceConfig),
